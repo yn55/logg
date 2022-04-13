d2710d905154f5798f884698878af57924e7e5b0
kaw393939 <keith@webizly.com>
2022-04-13 13:53:09 -0400

    Registration with an Observer

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,10 @@
 """A simple flask web app"""
-import flask_login
+import logging
 import os
-import datetime
-import time
+from logging.handlers import RotatingFileHandler
 
-from flask import g, request
-from rfc3339 import rfc3339
-
-from flask import render_template, Flask, has_request_context, request
+import flask_login
+from flask import Flask
 from flask_bootstrap import Bootstrap5
 from flask_wtf.csrf import CSRFProtect
 
@@ -17,80 +14,41 @@ from app.cli import create_database, create_log_folder
 from app.context_processors import utility_text_processors
 from app.db import db
 from app.db.models import User
-from app.exceptions import http_exceptions
+from app.error_handlers import error_handlers
+from app.logging_config import log_con
 from app.simple_pages import simple_pages
-import logging
-from flask.logging import default_handler
-from app.log_formatters import RequestFormatter
+
 login_manager = flask_login.LoginManager()
-
-
-def page_not_found(e):
-    return render_template("404.html"), 404
 
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
-    app.secret_key = 'This is an INSECURE secret!! DO NOT use this in production!!'
+    if app.config["ENV"] == "production":
+        app.config.from_object("app.config.ProductionConfig")
+    elif app.config["ENV"] == "development":
+        app.config.from_object("app.config.DevelopmentConfig")
+    elif app.config["ENV"] == "testing":
+        app.config.from_object("app.config.TestingConfig")
+
     login_manager.init_app(app)
     login_manager.login_view = "auth.login"
     csrf = CSRFProtect(app)
     bootstrap = Bootstrap5(app)
+    app.register_blueprint(log_con)
     app.register_blueprint(simple_pages)
     app.register_blueprint(auth)
+    app.register_blueprint(error_handlers)
     app.context_processor(utility_text_processors)
-    app.config['BOOTSTRAP_BOOTSWATCH_THEME'] = 'Simplex'
-    app.register_error_handler(404, page_not_found)
-    # app.add_url_rule("/", endpoint="index")
-    db_dir = "database/db.sqlite"
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.abspath(db_dir)
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.init_app(app)
     # add command function to cli commands
     app.cli.add_command(create_database)
     app.cli.add_command(create_log_folder)
+    db.init_app(app)
+    # Run once at startup:
 
-    # Deactivate the default flask logger so that log messages don't get duplicated
-    app.logger.removeHandler(default_handler)
 
-    # get root directory of project
-    root = os.path.dirname(os.path.abspath(__file__))
-    # set the name of the apps log folder to logs
-    logdir = os.path.join(root, 'logs')
-    # make a directory if it doesn't exist
-    if not os.path.exists(logdir):
-        os.mkdir(logdir)
-    # set name of the log file
-    log_file = os.path.join(logdir, 'info.log')
 
-    handler = logging.FileHandler(log_file)
-    # Create a log file formatter object to create the entry in the log
-    formatter = RequestFormatter('%(levelname)s,%(asctime)s,%(module)s,%(message)s,%(remote_addr)s,%(url)s,%(request_method)s,'
-        '%(request_path)s,%(ip)s,%(host)s,%(args)s'
-    )
-
-    # set the formatter for the log entry
-    handler.setFormatter(formatter)
-    # Set the logging level of the file handler object so that it logs INFO and up
-    handler.setLevel(logging.INFO)
-    # Add the handler for the log entry
-    app.logger.addHandler(handler)
-
-    @app.before_request
-    def start_timer():
-        g.start = time.time()
-
-    @app.after_request
-    def log_request(response):
-        if request.path == '/favicon.ico':
-            return response
-        elif request.path.startswith('/static'):
-            return response
-        elif request.path.startswith('/bootstrap'):
-            return response
-        app.logger.info("none")
-        return response
     return app
+
 
 @login_manager.user_loader
 def user_loader(user_id):
