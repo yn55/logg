caac5695d2de47f2293b0d198859a8ca79244e44
yoshi <yn55@njit.edu>
2022-04-29 23:32:26 -0400

    now it should work!

--- tests/user_test.py
+++ tests/user_test.py
@@ -7,17 +7,17 @@ from faker import Faker
 def test_adding_user(application):
     log = logging.getLogger("myApp")
     with application.app_context():
-        assert db.session.query(User).count() == 0
+        assert db.session.query(User).count() == 1
         assert db.session.query(Song).count() == 0
         #showing how to add a record
         #create a record
-        user = User('yn55@njit.edu', 'Yoshinori')
+        #user = User('yn55@njit.edu', 'Yoshinori')
         #add it to get ready to be committed
-        db.session.add(user)
+       # db.session.add(user)
         #call the commit
         #db.session.commit()
         #assert that we now have a new user
-        #assert db.session.query(User).count() == 1
+        assert db.session.query(User).count() == 1
         #finding one user record by email
         user = User.query.filter_by(email='yn55@njit.edu').first()
         log.info(user)
